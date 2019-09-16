13b0b5e827c0254006decfa09604ce2dc891f4f2
John Asper <agentx1994@gmail.com>
2019-09-16 18:52:16 -0400

    Make demo respect audio format's channel count

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,16 @@ fn main() {
 
     println!("Audio format: {:?}", format);
 
-    let mut osc = WaveTableOscillator::new(500.0, sample_rate, SAW_WAVE_TABLE.clone());
-    println!("{:?}", osc);
+    let mut oscs: Vec<WaveTableOscillator> = vec![];
+    for i in 0..format.channels {
+        let osc = WaveTableOscillator::new(
+            500.0 + 100.0 * (i as f64),
+            sample_rate,
+            SAW_WAVE_TABLE.clone(),
+        );
+        println!("Osc {}: {:?}\n", i, osc);
+        oscs.push(osc);
+    }
 
     thread::spawn(move || {
         event_loop.run(move |stream_id, stream_result| {
@@ -49,24 +57,35 @@ fn main() {
                 StreamData::Output {
                     buffer: UnknownTypeOutputBuffer::U16(mut buffer),
                 } => {
-                    //let next_value = osc.step().0;
-                    let next_value = osc.step();
                     for sample in buffer.chunks_mut(format.channels as usize) {
-                        let value = ((next_value * 0.5 + 0.5) * std::u16::MAX as f64) as u16;
+                        let next_values: Vec<f64> =
+                            oscs.iter_mut().map(&WaveTableOscillator::step).collect();
+                        let mut values = next_values
+                            .iter()
+                            .map(|x| ((x * 0.5 + 0.5) * std::u16::MAX as f64) as u16);
                         for out in sample.iter_mut() {
-                            *out = value;
+                            match values.next() {
+                                Some(x) => *out = x,
+                                None => *out = std::u16::MAX / 2,
+                            }
                         }
                     }
                 }
                 StreamData::Output {
                     buffer: UnknownTypeOutputBuffer::I16(mut buffer),
                 } => {
-                    //let next_value = osc.step().0;
-                    let next_value = osc.step();
                     for sample in buffer.chunks_mut(format.channels as usize) {
-                        let value = (next_value * std::i16::MAX as f64) as i16;
+                        let next_values: Vec<f64> =
+                            oscs.iter_mut().map(&WaveTableOscillator::step).collect();
+
+                        let mut values = next_values
+                            .iter()
+                            .map(|x| (x * std::i16::MAX as f64) as i16);
                         for out in sample.iter_mut() {
-                            *out = value;
+                            match values.next() {
+                                Some(x) => *out = x,
+                                None => *out = 0,
+                            }
                         }
                     }
                 }
@@ -74,20 +93,27 @@ fn main() {
                     buffer: UnknownTypeOutputBuffer::F32(mut buffer),
                 } => {
                     for sample in buffer.chunks_mut(format.channels as usize) {
-                        //let next_value = osc.step().0;
-                        let next_value = osc.step();
-                        let value = next_value as f32;
+                        let next_values: Vec<f64> =
+                            oscs.iter_mut().map(&WaveTableOscillator::step).collect();
+
+                        let mut values = next_values.iter().map(|x| *x as f32);
                         for out in sample.iter_mut() {
-                            *out = value;
+                            match values.next() {
+                                Some(x) => *out = x,
+                                None => *out = 0.0,
+                            }
                         }
                     }
                 }
                 _ => (),
             }
-            osc.set_frequency(osc.get_frequency() * 1.01);
-            if osc.get_frequency() > (osc.get_sample_rate() / 2) as f64 {
-                osc.set_frequency(osc.get_frequency() - (osc.get_sample_rate() / 2) as f64);
-            }
+
+            oscs.iter_mut().for_each(|osc| {
+                osc.set_frequency(osc.get_frequency() * 1.01);
+                if osc.get_frequency() > (osc.get_sample_rate() / 2) as f64 {
+                    osc.set_frequency(osc.get_frequency() - (osc.get_sample_rate() / 2) as f64);
+                }
+            });
         });
     });
 
